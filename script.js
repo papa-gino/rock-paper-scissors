821823960055aacaa9584cede12932e51405f621
papa-gino <papagino.stew@gmail.com>
2024-12-22 23:11:16 -0500

    Add computer selection function

---
+++ script.js
@@ -0,0 +1,19 @@
+function computerSelect() {
+    let choice;
+    switch(Math.floor(Math.random(3))) {
+        case 0:
+            choice = "rock";
+            break;
+        case 1:
+            choice = "paper";
+            break;
+        case 2:
+            choice = "scissors"
+            break;
+    }
+
+    return choice;
+}
+
+
+console.log(computerSelect())
